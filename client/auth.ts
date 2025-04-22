8a83532fec17e79ffd6557062473badecb43de2d
Cole Crown <aderibigbeadedamolajohn@gmail.com>
2025-04-22 12:47:27 +0100

    auth error fixed

--- client/auth.ts
+++ client/auth.ts
@@ -2,12 +2,7 @@ import NextAuth from "next-auth";
 
 import authConfig from "./auth.config";
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
