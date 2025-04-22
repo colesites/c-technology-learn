7aac4620d98e10899576e903fb51d5c1fe38d9ad
Cole Crown <aderibigbeadedamolajohn@gmail.com>
2025-04-22 12:31:51 +0100

    fix

--- client/src/components/auth/ForgotPasswordForm.tsx
+++ client/src/components/auth/ForgotPasswordForm.tsx
@@ -35,7 +35,7 @@ const ForgotPasswordForm = () => {
           </div>
 
           <p className="text-center mt-5 text-sm text-zinc-400">
-            Enter your email address and we'll send you a link to reset your
+            Enter your email address and we&apos;ll send you a link to reset your
             password.
           </p>
         </div>
